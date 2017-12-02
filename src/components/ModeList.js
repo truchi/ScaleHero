a2bf9177db4ac3afd77701781b11d8b0a807e57c
Romain TRUCHI <romain.truchi.06@gmail.com>
2017-12-03 00:32:52 +0100

    Rm ModeList's wrap

--- src/components/ModeList.js
+++ src/components/ModeList.js
@@ -5,20 +5,18 @@ class ModeList extends Component {
   render() {
     return (
       <div className="ModeList">
-        <div className="wrap">
-          {this.props.name &&
-            <div className="name">
-              {this.props.name}
-            </div>
-          }
-          {this.props.modes.map((mode, i) =>
-            <Mode
-              key={i}
-              mode={mode}
-              onChange={this.props.onChange}
-            />
-          )}
-        </div>
+        {this.props.name &&
+          <div className="name">
+            {this.props.name}
+          </div>
+        }
+        {this.props.modes.map((mode, i) =>
+          <Mode
+            key={i}
+            mode={mode}
+            onChange={this.props.onChange}
+          />
+        )}
       </div>
     )
   }
