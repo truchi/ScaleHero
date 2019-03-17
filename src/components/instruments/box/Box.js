181f648e20de2c3fec7ad86d42bc56c9b706d00a
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-03-17 12:31:53 +0100

    Fixes radius when leaving

--- src/components/instruments/box/Box.js
+++ src/components/instruments/box/Box.js
@@ -5,12 +5,15 @@ import styles from './Box.module.css'
 const Svg = rcv(<svg />)
 
 class Box extends Component {
+    prevRadius = '0%'
+
     render() {
         let { name, duration, href } = this.props
         let radius = this.props.styles(name).radius
-        radius = radius ? radius / 2 + '%' : '0%'
+        radius = radius ? radius / 2 + '%' : this.prevRadius
         href   = href(name)
 
+        this.prevRadius = radius
         return (
             <box className={ styles.box }>
                 <Svg className={ styles.svg } rcv={{ radius, duration }}>
