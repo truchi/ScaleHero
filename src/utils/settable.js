26ebf2304bd2d412fe980785c6470017260d0c9d
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-03-21 20:56:17 +0100

    settable: validate can return sanitized object

--- src/utils/settable.js
+++ src/utils/settable.js
@@ -5,14 +5,20 @@ export default ({ DEFAULTS = {}, validate = _ => true, Base = class {}, prefix =
   }
 
   set(props) {
-    const that = {}
+    const object = {}
     Object.entries(DEFAULTS)
       .forEach(([name, value]) =>
-        that[name] = [props[name], this[prefix + name], value].find(value => value !== undefined)
+        object[name] = [props[name], this[prefix + name], value].find(value => value !== undefined)
       )
 
-    validate(that)
-      && Object.entries(that)
+    let valid = validate(object)
+    if (valid === false || valid === null)
+      throw new Error('Invalid props', props)
+
+    if (valid === true || valid === undefined)
+      valid = object
+
+    Object.entries(valid)
         .forEach(([name, value]) => this[prefix + name] = value)
 
     this._afterSet && this._afterSet()
