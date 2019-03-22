5f088a6d0d676841155dccea76d8a01b42e17323
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-03-22 03:08:54 +0100

    InstrumentMask: position as in's arg

--- src/lib/instruments/Mask.js
+++ src/lib/instruments/Mask.js
@@ -1,7 +1,6 @@
 import settable from '../../utils/settable'
 
 const DEFAULTS = {
-  position  : 0,
   definition: _ => []
 }
 
@@ -9,21 +8,17 @@ export default class Mask extends settable({ DEFAULTS }) {
   _position
   _definition
 
-  constructor({ position, definition } = {}) {
-    super({ position, definition })
+  constructor({ definition } = {}) {
+    super({ definition })
   }
 
-  in(i = 0, j = 0) {
-    const def = this._def[i]
+  in(position = 0, i = 0, j = 0) {
+    const def = this._definition(position)[i]
 
     return def && (-1 !== def.findIndex(([min, max]) => min <= j && j <= max))
   }
 
-  get _def() {
-    return this._definition(this._position)
-  }
-
   static in(masks = [], i = 0, j = 0) {
-    return -1 !== masks.findIndex(mask => mask.in(i, j))
+    return -1 !== masks.findIndex(({ position, mask }) => mask.in(position, i, j))
   }
 }
