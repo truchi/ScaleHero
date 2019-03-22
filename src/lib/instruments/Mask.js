39f59032f61ba92ccb32a676ee4e7555d06d590c
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-03-22 14:24:25 +0100

    Mask: in renamed as inside (reserved kw...)

--- src/lib/instruments/Mask.js
+++ src/lib/instruments/Mask.js
@@ -12,15 +12,15 @@ export default class Mask extends settable({ DEFAULTS }) {
     super({ definition })
   }
 
-  in(position = 0, i = 0, j = 0) {
+  inside(position = 0, i = 0, j = 0) {
     const def = this._definition(position)[i]
 
     return def && (-1 !== def.findIndex(([min, max]) => min <= j && j <= max))
   }
 
-  static in(masks = [], i = 0, j = 0) {
+  static inside(masks = [], i = 0, j = 0) {
     return masks.length
-      ? -1 !== masks.findIndex(({ position, mask }) => mask.in(position, i, j))
+      ? -1 !== masks.findIndex(({ position, mask }) => mask.inside(position, i, j))
       : true
   }
 }
