7fb7bb05759df1fd76ea7ba4e86d46866129b781
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-04-05 22:25:49 +0200

    (removed import to styles)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import './styles/index.css'
 import registerServiceWorker from './registerServiceWorker'
 
 import './utils/polyfills'
