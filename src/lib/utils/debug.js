23e3ec393344d34eb9b14fec473ebdfeb2dbebf0
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-04-09 01:48:53 +0200

    (comments)

--- src/lib/utils/debug.js
+++ src/lib/utils/debug.js
@@ -1,1 +1,3 @@
+//** Logs scoped arguments and own argument and returns own argument
+//:: (...args) -> arg -> arg
 export default (...args) => arg => console.log(...args, arg) || arg
--- src/lib/utils/randId.js
+++ src/lib/utils/randId.js
@@ -1,1 +1,3 @@
+//** Returns a random id of length length
+//:: Number length -> () -> String id
 export default l => () => Math.random().toString(36).substr(2, l)
