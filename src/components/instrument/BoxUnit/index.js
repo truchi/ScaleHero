9e28fa2ad02a3d45b289eed750a05035eef5f15c
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-04-12 12:34:15 +0200

    (rename)

--- src/components/instrument/BoxUnit/index.js
+++ src/components/instrument/BoxUnit/index.js
@@ -2,14 +2,14 @@ import React  from 'react'
 import rcv    from '../../../lib/rcv'
 import styles from './styles.module.scss'
 
-const Unit = rcv(<rect />)
+const Rect = rcv(<rect />)
 
 const defaults = {
   color: 'transparent',
 }
 
 export default ({ style, clip }) => (
-  <Unit
+  <Rect
     className={ styles.unit }
     rcv={{ ...defaults, ...style, clip }}
   />
