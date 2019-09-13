b4499b75bc3f4dcc8cf37a256cb1f1ce85451583
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-09-13 14:42:11 +0200

    Iterators now have cursor at current item

--- src/lib/grid/utils/ArrayIterator.js
+++ src/lib/grid/utils/ArrayIterator.js
@@ -1,4 +1,8 @@
 // RepeatableArray = { count: Number, repeat: Array }
+import {
+  clamp,
+  clone,
+} from 'ramda'
 
 /**
  * Iterates over repeat, count times
@@ -7,25 +11,28 @@
 export default ({ count = 1, repeat = [] }) => {
   let   iterator
   const bounds = {
-    index: repeat.length,
-    count
+    min: {
+      index: -1,
+      count:  1
+    },
+    max: {
+      index: repeat.length - 1,
+      count
+    }
   }
 
-  const forward = () => ++cursor.index                       // Increments index
-  const start   = () => ((cursor.index = 0, ++cursor.count)) // Increments count and resets index
-  const get     = () => repeat[cursor.index]                 // Returns current value
+  const forward = () => ++cursor.index                        // Increments index
+  const start   = () => ((cursor.index = -1, ++cursor.count)) // Increments count and resets index
+  const get     = () => repeat[cursor.index]                  // Returns current value
 
-  const moveForward = () => (value => ((value = get(), forward(), { value })))() // Retuns current value and forwards
-  const moveStart   = () => ((start(), moveForward()))                           // Loops, return current value and forwards
+  const moveForward = () => ((forward(), { value: get() }))   // Retuns current value and forwards
+  const moveStart   = () => ((start(), moveForward()))        // Loops, return current value and forwards
 
   /**
    * Cursor
    * @kind { index: Number, count: Number }
    */
-  const cursor = {
-    index: 0,
-    count: 1,
-  }
+  const cursor = clone(bounds.min)
 
   /**
    * Iterator
@@ -36,14 +43,14 @@ export default ({ count = 1, repeat = [] }) => {
      * Returns cursor
      * @kind () => Cursor cursor
      */
-    cursor: () => Object.assign({}, cursor),
+    cursor: () => clone(cursor),
     /**
      * Moves cursor to ith item, cth repeat
      * @kind Number i, Number c -> Iterator iterator
      */
-    goto: ({ index = cursor.index, count = cursor.count } = {}) => ((
-      cursor.index = Math.min(index, bounds.index),
-      cursor.count = Math.min(count, bounds.count),
+    goto: ({ index = bounds.min.index, count = bounds.min.count } = {}) => ((
+      cursor.index = clamp(bounds.min.index, bounds.max.index, index),
+      cursor.count = clamp(bounds.min.count, bounds.max.count, count),
       iterator
     )),
     /**
@@ -52,8 +59,8 @@ export default ({ count = 1, repeat = [] }) => {
      * @kind () -> Object { value, done }
      */
     next: () => {
-      const canForward = cursor.index < bounds.index
-      const canRepeat  = cursor.count < bounds.count
+      const canForward = cursor.index < bounds.max.index
+      const canRepeat  = cursor.count < bounds.max.count
       const isDone     = !canForward && !canRepeat
 
       return isDone     ? { done: true }
--- src/lib/grid/utils/GroupIterator.js
+++ src/lib/grid/utils/GroupIterator.js
@@ -1,15 +1,14 @@
 import ArrayIterator from './ArrayIterator'
 import {
-  adjust,
   append,
   call,
+  clone,
   compose as c,
   last,
   map,
-  pick,
   prop,
   reduce,
-  tap,
+  reduced,
 } from 'ramda'
 
 // Group = RecursiveRepeatableArray = { count: Number, repeat: Group[] }
@@ -26,9 +25,12 @@ import {
  * Iterates over a group
  * @kind Group group -> Iterator iterator
  */
-export default ({ count, repeat }) => {
+export default ({ count = 1, repeat = [] }) => {
   let iterator
-  let stack = [ArrayIterator({ count, repeat })] // Iterator stack
+  let stack  = [ArrayIterator({ count, repeat })]
+  let cursor = []
+
+  const getCursor = () => cursor = map(c(call, prop('cursor')))(stack)
 
   const pop  = ()    => ((stack.pop()                     , iterator.next()))
   const push = value => ((stack.push(ArrayIterator(value)), iterator.next()))
@@ -38,29 +40,31 @@ export default ({ count, repeat }) => {
      * Returns cursor
      * @kind () => Cursor cursor
      */
-    cursor: () => map(pick(['index', 'repeat']), stack),
+    cursor: () => clone(cursor),
     /**
      * Moves cursor
      * @kind Cursor cursor -> Iterator iterator
      */
-    goto: (cursor = [{}])=> ((
+    goto: (cursor = [])=> ((
       stack = c(
         prop('stack'),
-        reduce(                                                    // Construct iterator stack
-          ({ stack, current }, { index, count } = {}) =>
-            ({
-              stack: c(
-                append(ArrayIterator(current).goto(index, count)), // Append array iterator moved to the given index & repeat
-                adjust(-1, tap(c(call, prop('next')))),            // Call next on previous iterator
-                                                                   //   (user gives a path to the current position,
-                                                                   //   so all array iterator --except for the last one--
-                                                                   //   have to be nexted)
-              )(stack),
-              current: current.repeat[index]                       // new current is indexth child of old current
-            }),
-          { stack: [], current: { count, repeat } }                // Initialize with empty stack and initial group
+        reduce(
+          (
+            { stack, current },
+            cursor,
+            iterator = ArrayIterator(current).goto(cursor),
+            index    = iterator.cursor().index
+          ) =>
+            current.repeat
+              ? ({
+                stack: append(iterator)(stack), // append array iterator moved to the given index & repeat
+                current: current.repeat[index], // new current is indexth child of old current
+              })
+              : reduced({ stack, current }),    // cannot go further
+          { stack: [], current: { count, repeat } }
         )
-      )(cursor),
+      )(cursor.length ? cursor : [{}]),
+      getCursor(),
       iterator
     )),
     /**
@@ -70,7 +74,6 @@ export default ({ count, repeat }) => {
      */
     next: () => {
       const it = last(stack)
-
       if (!it) return { done: true }
 
       let { value, done } = it.next()
@@ -78,7 +81,7 @@ export default ({ count, repeat }) => {
 
       return done      ? pop()
            : recursive ? push(value)
-                       : { value }
+                       : ((getCursor(), { value }))
     },
   })
 }
