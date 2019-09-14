eb0838a266bf06e83c47c806fd1bd43dcd672e59
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-09-14 14:00:12 +0200

    Grid: index

--- src/lib/grid/index.js
+++ src/lib/grid/index.js
@@ -1,11 +1,29 @@
 import _group         from './utils/group'
 import groupIterator  from './utils/GroupIterator'
 import groupsIterator from './utils/GroupsIterator'
+import {
+  append,
+  compose as c,
+  reduce,
+} from 'ramda'
 
-const getItem = (item, index) => item[['sections', 'lines', 'bars', 'items'][index.length]]
-const isItem  = (item, index) => index.length === 4
+export const group = _group(
+  (item, index) => item[['sections', 'lines', 'bars', 'items'][index.length]],
+  (item, index) => index.length === 4
+)
 
-export const group = _group(getItem, isItem)
+export const index =
+  (grid, current = group(grid)) =>
+    c(
+      ({ current: { index }, repeats }) => ({ index, repeats }),
+      reduce(
+        ({ current: { startIndex, endIndex, repeat }, repeats }, { index, count }) => ({
+          current: repeat[index],
+          repeats: append({ startIndex, endIndex, count }, repeats),
+        }),
+        { current, repeats: [] },
+      )
+    )
 
 export {
   groupIterator,
