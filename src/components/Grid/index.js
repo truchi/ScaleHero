970137eaf0e05d52808a23f2358553a875ccbc66
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-09-14 16:41:11 +0200

    Grid component: a fabulous start

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,11 +1,41 @@
 import React       from 'react'
 import { connect } from 'react-redux'
-/* import styles      from './styles.module.scss' */
+import styles      from './styles.module.scss'
 
 export default connect(
   state => state,
 )(
-  ({ grid, index }) => (
-    <div></div>
+  ({ grid, index: { index, repeats } }) => (
+    <grid className={ styles.grid }>
+
+      { grid.sections.map((section, key) => (
+        <section className={ styles.section } key={ key }>
+
+          { section.lines.map((line, key) => (
+            <line className={ styles.line } key={ key }>
+
+              { line.bars.map(({ repeat, count, items }, key) => (
+                <bar className={ styles.bar } key={ key } start={ repeat ? '' : null } end={ count > 1 ? '' : null }>
+
+                  <items className={ styles.items }>
+                    { items.map(({ chord, duration }, key) => (
+                      <item className={ styles.item } style={{ flexGrow: duration }} key={ key }>
+                        <chord className={ styles.chord }>{ chord }</chord>
+                        <duration className={ styles.duration } duration={ duration }></duration>
+                      </item>
+                    )) }
+                  </items>
+
+                  <repeat></repeat>
+                </bar>
+              )) }
+
+            </line>
+          )) }
+
+        </section>
+      )) }
+
+    </grid>
   )
 )
