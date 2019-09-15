1320558fda8de629f5eb3a451b19775d4092e953
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-09-15 17:24:01 +0200

    Grid

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -1,27 +1,57 @@
 import React       from 'react'
 import { connect } from 'react-redux'
 import styles      from './styles.module.scss'
+import {
+  addIndex,
+  reduce,
+  find,
+} from 'ramda'
 
-const highlight = (index, stack) =>
-  stack.reduce(
+const is = (index, current) =>
+  addIndex(reduce)(
     (highlight, ind, i) => highlight && (index[i] === ind),
-    true
-  ) ? '' : null
+    true,
+    current
+  )
+
+const highlight = (index, current) => is(index, current) ? '' : null
+
+const getCount = (repeats, current) =>
+  ((repeat = find(
+      ({ endIndex }) => is(endIndex, current),
+      repeats
+  )) =>
+    repeat && repeat.count
+  )()
+
+const Repeat = ({ repeats, current, count }) =>
+  count > 1 && (
+    <repeat>
+      { ((c = getCount(repeats, current)) =>
+        c && (<span>({ c })</span>)
+      )() }
+      <span>{ `x${ count }` }</span>
+    </repeat>
+  )
 
 export default connect(
   state => state,
 )(
-  ({ grid, index: { index = [0, 0, 0, 0], repeats } }) => (
+  ({ grid: { sections, count }, index: { index = [], repeats } }) => (
     <grid className={ styles.grid }>
 
-      { grid.sections.map((section, s) => (
+      { sections.map(({ name, lines, count }, s) => (
         <section
           key={ s }
           className={ styles.section }
           highlight={ highlight(index, [s]) }
         >
+          { name && (
+            <name>{ name }</name>
+          )}
+          <Repeat repeats={ repeats } current={ [s] } count={ count }></Repeat>
 
-        { section.lines.map((line, l) => (
+        { lines.map((line, l) => (
             <line
               key={ l }
               className={ styles.line }
@@ -51,7 +81,7 @@ export default connect(
                     )) }
                   </chords>
 
-                  <repeat>{ count > 1 && `x${ count }` }</repeat>
+                  <Repeat repeats={ repeats } current={ [s, l, b] } count={ count }></Repeat>
                 </bar>
               )) }
 
