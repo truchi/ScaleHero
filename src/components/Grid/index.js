141b44954771fff03005c70d9902a30980aa5119
Romain TRUCHI <romain.truchi.06@gmail.com>
2019-09-15 15:42:53 +0200

    Grid: highlights

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -2,24 +2,49 @@ import React       from 'react'
 import { connect } from 'react-redux'
 import styles      from './styles.module.scss'
 
+const highlight = (index, stack) =>
+  stack.reduce(
+    (highlight, ind, i) => highlight && (index[i] === ind),
+    true
+  ) ? '' : null
+
 export default connect(
   state => state,
 )(
-  ({ grid, index: { index, repeats } }) => (
+  ({ grid, index: { index = [0, 0, 0, 0], repeats } }) => (
     <grid className={ styles.grid }>
 
-      { grid.sections.map((section, key) => (
-        <section className={ styles.section } key={ key }>
+      { grid.sections.map((section, s) => (
+        <section
+          key={ s }
+          className={ styles.section }
+          highlight={ highlight(index, [s]) }
+        >
 
-          { section.lines.map((line, key) => (
-            <line className={ styles.line } key={ key }>
+        { section.lines.map((line, l) => (
+            <line
+              key={ l }
+              className={ styles.line }
+              highlight={ highlight(index, [s, l]) }
+            >
 
-              { line.bars.map(({ repeat, count, items }, key) => (
-                <bar className={ styles.bar } key={ key } start={ repeat ? '' : null } end={ count > 1 ? '' : null }>
+            { line.bars.map(({ repeat, count, items }, b) => (
+                <bar
+                  key={ b }
+                  className={ styles.bar }
+                  start={ repeat ? '' : null }
+                  end={ count > 1 ? '' : null }
+                  highlight={ highlight(index, [s, l, b]) }
+                >
 
                   <items className={ styles.items }>
-                    { items.map(({ chord, duration }, key) => (
-                      <item className={ styles.item } style={{ flexGrow: duration }} key={ key }>
+                    { items.map(({ chord, duration }, i) => (
+                      <item
+                        className={ styles.item }
+                        style={{ flexGrow: duration }}
+                        key={ i }
+                        highlight={ highlight(index, [s, l, b, i]) }
+                      >
                         <chord className={ styles.chord }>{ chord }</chord>
                         <duration className={ styles.duration } duration={ duration }></duration>
                       </item>
